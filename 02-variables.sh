d16628e5db4f7db1e60b9b246c8f8ad7592c9649
NarendraReddy <narendra.nya@gmail.com>
2024-04-21 05:26:51 -0700

    01-hello-commit

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::Hi Suresh,how are you"
+echo "Suresh::Hi Ramesh,i am fine. How are you"
+echo "Ramesh::i m good. How about your ofc"
+echo "Suresh::Good.Planning to take devops course"
