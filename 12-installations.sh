8b3ff0c8c2f58c9e0e1a7e47c7182bd5fe22c401
NarendraReddy <narendra.nya@gmail.com>
2024-04-21 08:19:39 -0700

    11forloop

--- 12-installations.sh
+++ 12-installations.sh
@@ -3,8 +3,13 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
-echo -e "$R RED IN COLOR"
+echo "Green :$G then $N"
+
+
 
 USERID=$(id -u)
 
