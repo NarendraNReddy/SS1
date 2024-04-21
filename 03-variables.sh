a64ae52ae51e5221feab90bd01ec209d0746ac3e
NarendraReddy <narendra.nya@gmail.com>
2024-04-21 05:28:42 -0700

    03-variablles

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=RAMESH
+PERSON2=SURESH
+
+echo "$PERSON1::Hi $PERSON2,how are you"
+echo "$PERSON2::Hi $PERSON1,i am fine. How are you"
+echo "$PERSON1::i m good. How about your ofc"
+echo "$PERSON2::Good.Planning to take devops course"
