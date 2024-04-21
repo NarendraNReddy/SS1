582ccded6c2f71f5d9553f766fd808b7af4e1168
NarendraReddy <narendra.nya@gmail.com>
2024-04-21 05:42:59 -0700

    08-specvariablles

---
+++ 08-specialvaribles.sh
@@ -0,0 +1,14 @@
+#/usr/bin
+
+echo "All cmd line args:$@"
+echo "no of cmd line args:$#"
+echo "Prog name:$0"
+
+echo "username:$USER"
+echo "current dir:$PWD"
+echo "Home : $HOME"
+
+echo "hostname :$HOSTNAME"
+echo "Current PID:$$"
+sleep 60
+echo "Background PID:$!"
